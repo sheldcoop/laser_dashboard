4bfa6200197612e775d965b9d9479a072b81b1b3
sheldcoop <106275963+sheldcoop@users.noreply.github.com>
2025-09-01 12:00:44 +0200

    Update app.py

--- laser_calculator_app/app.py
+++ laser_calculator_app/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from modules import (
     home, process_recommender, material_analyzer, liu_plot_analyzer, 
     thermal_effects_calculator, beam_profile_visualizer, mask_finder, 
-    pulse_energy_calculator, fluence_calculator, dose_target_seeker
+    pulse_energy_calculator, fluence_calculator, dose_target_seeker,
     documentation  # <-- 1. Import the new documentation module
 )
 
